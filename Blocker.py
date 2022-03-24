d827ace3ea65bb72b6556a4ce44f032043e3a105
Leonard van der Plaat <30744870+lplaat@users.noreply.github.com>
2022-03-24 16:57:12 +0100

    Update Blocker.py

--- Blocker.py
+++ Blocker.py
@@ -1,42 +1,111 @@
 import random
+import json
 
-def incrypt(msg):
-    Salt = random.randint(10**15, 10**16)
-    msg_list = []
-    msg_salte = []
+def gen_map(seed, max):
+    random.seed(seed)
+    map = []
+    for y in range(max[1]):
+        map.append([])
+        for _ in range(max[0]):
+            map[y].append(random.randint(0, 1114111))
 
-    abc = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5','6','7', '8', '9']
+    return map
 
-    for part in msg:
-        for letter_id in range(len(abc)):
-            if part.lower() == abc[letter_id].lower():
-                msg_list.append(letter_id)
+def step(i, seed, step_map, max):
+    steps_map = json.loads(json.dumps(step_map))
+    go = True
+    while go:
+        random.seed(seed)
 
-    part_seed = Salt
-    for i in msg_list:
-        random.seed(part_seed)
-        part_seed = random.randint(10**15, 10**16)
-        random.seed(part_seed)
-        msg_salte.append(i+random.randint(0, 20))
-    return [Salt, msg_salte]
+        for _ in range(len(steps_map)):
+            t_x = random.randint(0, max[0]-1)
+            t_y = random.randint(0,  max[1]-1)
 
-def decrypt(Salt, msg):
-    try:
-        abc = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5','6','7', '8', '9']
-        decrypt_msg_array = []
-        part_seed = Salt
-        msg_string = ""
+        t_out = [random.randint(0,  max[0]-1), random.randint(0, max[1]-1)]
 
-        for i in msg:
-            random.seed(part_seed)
-            part_seed = random.randint(10**15, 10**16)
-            random.seed(part_seed)
-            decrypt_msg_array.append(i - random.randint(0, 20))
+        found = False
+        for stemp in steps_map:
+            if json.dumps(stemp) == json.dumps(t_out):
+                found = True
+                steps_map.append([None, None])
+        
+        if found == False:
+            go = False
+            steps_map.append(t_out)
+            return [t_out, steps_map]
 
-        for part in decrypt_msg_array:
-            for letter_id in range(len(abc)):
-                if part == letter_id:
-                    msg_string += abc[letter_id].lower()
-        return msg_string
-    except:
-        return "Decrypter faild"
+
+def incrypt_part(a, b, seed):
+    return a + b
+
+def decrypt_part(a, b, seed):
+    if a == 690 or a == 420:
+        return None
+    return a - b
+
+def incrypt_block(input, max):
+    input = [ord(character) for character in input]
+
+    if not (max[0] * max[1]) < len(input)+2:
+        seed = random.randint(10**15, 10**16)
+        map = gen_map(seed, max)
+
+        input.append(690)
+        input.append(420)
+
+        jumps = 0
+        sec = []
+        for part in input:
+            point = step(jumps, seed, sec, max)
+            sec = point[1]
+            map[point[0][1]][point[0][0]] = incrypt_part(part, map[point[0][1]][point[0][0]], seed)
+            jumps += 1
+
+        map_out = []
+        for y in range(max[1]):
+            map_out.append([])
+            for x in range(max[0]):
+                map_out[y].append(chr(map[y][x]))
+
+        return [seed, map_out]
+    else:
+        return 'To large to put in block!'
+
+def decrypt_block(input):
+    seed = input[0]
+    input = input[1]
+    clean_map = gen_map(seed, [len(input[0]), len(input)])
+    map = []
+
+    for y in range(len(input)):
+        map.append([])
+        for x in range(len(input[0])):
+            map[y].append(ord(input[y][x]))
+
+    string = []
+    jumps = 0
+    sec = []
+    go = True
+    past = None
+    while go:
+        point = step(jumps, seed, sec, [len(input[0]), len(input)])
+        sec = point[1]
+        jumps += 1
+
+        out_part = decrypt_part(map[point[0][1]][point[0][0]], clean_map[point[0][1]][point[0][0]], seed)
+        if not out_part == None:
+            string.append(out_part)
+
+        if past == 690 and decrypt_part(map[point[0][1]][point[0][0]], clean_map[point[0][1]][point[0][0]], seed) == 420:
+            go = False
+
+        past = decrypt_part(map[point[0][1]][point[0][0]], clean_map[point[0][1]][point[0][0]], seed)
+        if jumps > (len(input)*len(input[0]))-1:
+            print('No stopping code found')
+            go = False
+
+    out_string = ''
+    for part in string:
+        out_string += chr(part)
+
+    return out_string
